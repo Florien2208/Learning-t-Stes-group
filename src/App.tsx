1683be1140be39c328c1f4a2dab88df261fdf6c5
florien2208 <mahorompakanyiflorien@gmail.com>
2024-02-29 10:15:25 +0200

    small change

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import * as React from "react";
-import { createRoot } from "react-dom/client";
 import {
   createBrowserRouter,
-  createRoutesFromElements,
   RouterProvider,
 } from "react-router-dom";
-import Navbar from "./pages/Navbar";
 import Login from "./pages/Login";
 import HomePage from "./pages/HomePage";
 import About from "./pages/About";
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,3 @@
-// App.tsx
-
 import React from "react";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
